07d16f70146540225a1c04be8c4a045dbee6684e
greenhy <63488123+greenhy@users.noreply.github.com>
2020-07-29 14:48:38 +1000

    Update python_basic.py

--- python_basic.py
+++ python_basic.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
-
-
 #This code helps to print something
 print('I am now printing hello world!')
-
-
-# In[4]:
 
 
 #Variable creation
@@ -18,16 +12,10 @@ student_age = 22
 student_gpa = 4.5
 
 
-# In[5]:
-
-
 #print variables
 print('Student name is:', student_name)
 print('Student age is:', student_age)
 print('Student gpa is:', student_gpa)
-
-
-# In[7]:
 
 
 #function
@@ -37,13 +25,7 @@ def student_details(student_name, student_age, student_gpa):
     print('Student gpa is:', student_gpa)
 
 
-# In[9]:
-
-
 student_details('name2',23,4.0)
-
-
-# In[10]:
 
 
 #squares any input and sotres it in variable 'num'
@@ -51,9 +33,6 @@ def sqr(num):
     num = num*num
     return num
 print(sqr(2))
-
-
-# In[12]:
 
 
 #defind a function that can do factorial
@@ -64,14 +43,4 @@ def factorial(num):
     return result
 
 
-# In[13]:
-
-
 print(factorial(5))
-
-
-# In[ ]:
-
-
-
-
